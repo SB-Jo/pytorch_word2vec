74388670e56c5d5b077b46fb7322770c2a7ec020
yesys7777 <yesys7777@gmail.com>
2021-07-29 15:31:49 +0900

    change negative sampling

--- utils.py
+++ utils.py
@@ -37,6 +37,8 @@ def create_contexts_target(file_path, window_size, vocab, sample_size):
     p = np.power(p, 0.75)
     p /= np.sum(p)
 
+    n_sampler = NegativeSampler(vocab_size, window_size, sample_size, p)
+
     print('문맥-타겟 단어 생성 및 네거티브 샘플링 데이터 생성 시작')
     for c in corpus:
         for idx in range(window_size, len(c)-window_size):
@@ -50,7 +52,7 @@ def create_contexts_target(file_path, window_size, vocab, sample_size):
             targets.append(c[idx])
             contexts.append(cs)
             labels.append(1)
-            negative_sample = get_negative_sample(vocab_size, sample_size, c[idx], p)
+            negative_sample = n_sampler.get_negative_sample(c[idx])
             
             targets.append(c[idx])
             contexts.append(list(negative_sample))
@@ -61,9 +63,17 @@ def create_contexts_target(file_path, window_size, vocab, sample_size):
     print('문맥-타겟 단어 생성 및 네거티브 샘플링 데이터 생성 끝')
     return cs, labels
 
-def get_negative_sample(vocab_size, sample_size, target, p):
-    p = p.copy()
-    p[target] = 0
-    p /= p.sum()
-    negative_sample = np.random.choice(vocab_size, size=sample_size, replace=False, p=p)
-    return negative_sample
+
+class NegativeSampler:
+    def __init__(self, vocab_size, window_size, sample_size, p):
+        self.vocab_size = vocab_size
+        self.window_size = window_size
+        self.sample_size = sample_size
+        self.p = p
+
+    def get_negative_sample(self, target):
+        p = self.p.copy()
+        p[target] = 0
+        p /= p.sum()
+        negative_sample = np.random.choice(self.vocab_size, size=(self.sample_size, self.window_size*2), replace=False, p=p)
+        return negative_sample
